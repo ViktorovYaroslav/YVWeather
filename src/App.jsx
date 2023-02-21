6bfd2efd4b2bf84af212688a0820516b8ce3b073
Yaroslav Viktorov <viktorov.yaroslav@gmail.com>
2023-02-21 23:41:20 +0200

    v1
    The first version of the application without adaptive layout.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// components imports
+import { WeaderCard } from './components/weatherCard/WeaderCard';  
+
+// style imports
+import './style/App.scss';
+
+
+
+export const App = () => {
+
+
+   return (
+      <div className="app">
+         <div className="_wrapper">
+
+            <main className="main">
+               <div className="_container">
+                  <WeaderCard />
+               </div>
+            </main>
+
+         </div>
+      </div>
+   )
+}
